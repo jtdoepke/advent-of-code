201ead86c0f14efe474cbf42eba9926f96cdaf49
Jaye Doepke <jtdoepke@protonmail.com>
2021-12-09 22:57:14 -0600

    2021 day 7

--- 2021/day_07/main.go
+++ 2021/day_07/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -22,9 +25,72 @@ func main() {
 }
 
 func problemPart1(inp []byte) string {
-	return "NOT IMPLEMENTED"
+	positions := parseInput(inp)
+	var max int
+	for _, pos := range positions {
+		if pos > max {
+			max = pos
+		}
+	}
+	costs := make([]int, max+1)
+	for _, pos := range positions {
+		for i := 0; i < max+1; i++ {
+			costs[i] += abs(pos - i)
+		}
+	}
+	var minCost int
+	minCost = math.MaxInt
+	for _, cost := range costs {
+		if cost < minCost {
+			minCost = cost
+		}
+	}
+	return strconv.Itoa(minCost)
 }
 
 func problemPart2(inp []byte) string {
-	return "NOT IMPLEMENTED"
+	positions := parseInput(inp)
+	var max int
+	for _, pos := range positions {
+		if pos > max {
+			max = pos
+		}
+	}
+	costs := make([]int, max+1)
+	for _, pos := range positions {
+		for i := 0; i < max+1; i++ {
+			costs[i] += gaussSum(abs(pos - i))
+		}
+	}
+	var minCost int
+	minCost = math.MaxInt
+	for _, cost := range costs {
+		if cost < minCost {
+			minCost = cost
+		}
+	}
+	return strconv.Itoa(minCost)
+}
+
+func parseInput(inp []byte) (positions []int) {
+	inp = bytes.TrimSpace(inp)
+	b := bytes.NewBuffer(make([]byte, 0, len(inp)+2))
+	b.WriteByte('[')
+	b.Write(inp)
+	b.WriteByte(']')
+	if err := json.NewDecoder(b).Decode(&positions); err != nil {
+		log.Panic(err)
+	}
+	return
+}
+
+func abs(i int) int {
+	if i < 0 {
+		return -i
+	}
+	return i
+}
+
+func gaussSum(n int) int {
+	return (n * (n + 1)) / 2
 }
--- 2021/day_07/main_test.go
+++ 2021/day_07/main_test.go
@@ -23,10 +23,10 @@ func getTestData(t *testing.T) []byte {
 
 func Test_problemPart1(t *testing.T) {
 	got := problemPart1(getTestData(t))
-	assert.Equal(t, "NOT IMPLEMENTED", got)
+	assert.Equal(t, "37", got)
 }
 
 func Test_problemPart2(t *testing.T) {
 	got := problemPart2(getTestData(t))
-	assert.Equal(t, "NOT IMPLEMENTED", got)
+	assert.Equal(t, "168", got)
 }
