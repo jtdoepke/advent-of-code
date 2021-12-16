145cbc7c0ba9d2988cb1a1d51705a162a97a367f
Jaye Doepke <jtdoepke@protonmail.com>
2021-12-16 01:46:29 -0600

    2021 day 16

--- 2021/day_16/main.go
+++ 2021/day_16/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"bytes"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"os"
+	"strconv"
+
+	bitstream "github.com/dgryski/go-bitstream"
 )
 
 func main() {
@@ -22,9 +27,249 @@ func main() {
 }
 
 func problemPart1(inp []byte) string {
-	return "NOT IMPLEMENTED"
+	p := parseInput(inp)
+	var sumVersions func(*Packet) int
+	sumVersions = func(p *Packet) int {
+		c := int(p.Version)
+		for _, sp := range p.Subpackets {
+			c += sumVersions(sp)
+		}
+		return c
+	}
+	return strconv.Itoa(sumVersions(p))
 }
 
 func problemPart2(inp []byte) string {
-	return "NOT IMPLEMENTED"
+	p := parseInput(inp)
+	var evalPacket func(*Packet) int
+	evalPacket = func(p *Packet) int {
+		switch p.Type {
+		case PacketTypeSum:
+			sum := 0
+			for _, sp := range p.Subpackets {
+				sum += evalPacket(sp)
+			}
+			return sum
+
+		case PacketTypeProduct:
+			product := 1
+			for _, sp := range p.Subpackets {
+				product *= evalPacket(sp)
+			}
+			return product
+
+		case PacketTypeMinimum:
+			min := math.MaxInt
+			for _, sp := range p.Subpackets {
+				if v := evalPacket(sp); v < min {
+					min = v
+				}
+			}
+			return min
+
+		case PacketTypeMaximum:
+			max := math.MinInt
+			for _, sp := range p.Subpackets {
+				if v := evalPacket(sp); v > max {
+					max = v
+				}
+			}
+			return max
+
+		case PacketTypeLiteral:
+			return int(p.Literal)
+
+		case PacketTypeGreaterThan:
+			if evalPacket(p.Subpackets[0]) > evalPacket(p.Subpackets[1]) {
+				return 1
+			} else {
+				return 0
+			}
+
+		case PacketTypeLessThan:
+			if evalPacket(p.Subpackets[0]) < evalPacket(p.Subpackets[1]) {
+				return 1
+			} else {
+				return 0
+			}
+
+		case PacketTypeEqualTo:
+			if evalPacket(p.Subpackets[0]) == evalPacket(p.Subpackets[1]) {
+				return 1
+			} else {
+				return 0
+			}
+		}
+		log.Panic("unknown packet type", p.Type)
+		return 0
+	}
+
+	return strconv.Itoa(evalPacket(p))
+}
+
+func parseInput(inp []byte) *Packet {
+	r, err := hexToBitstream(bytes.TrimSpace(inp))
+	if err != nil {
+		log.Panic(err)
+	}
+	_, p, err := readPacket(r)
+	if err != nil {
+		log.Panic(err)
+	}
+	return p
+}
+
+type PacketType byte
+
+const (
+	PacketTypeSum PacketType = iota
+	PacketTypeProduct
+	PacketTypeMinimum
+	PacketTypeMaximum
+	PacketTypeLiteral
+	PacketTypeGreaterThan
+	PacketTypeLessThan
+	PacketTypeEqualTo
+)
+
+type PacketLengthType byte
+
+const (
+	PacketLengthTypeTotal      PacketLengthType = 0
+	PacketLengthTypeSubpackets PacketLengthType = 1
+)
+
+type Packet struct {
+	Version byte
+	Type    PacketType
+
+	Literal    uint64
+	Subpackets []*Packet
+}
+
+func readPacket(r *bitstream.BitReader) (int, *Packet, error) {
+	read := 0
+
+	version, err := r.ReadBits(3)
+	if err != nil {
+		return 0, nil, err
+	}
+	read += 3
+
+	pTypeInt, err := r.ReadBits(3)
+	if err != nil {
+		return 0, nil, err
+	}
+	pType := PacketType(pTypeInt)
+	read += 3
+
+	p := &Packet{
+		Version: byte(version),
+		Type:    pType,
+	}
+
+	switch pType {
+	case PacketTypeLiteral:
+		n, v, err := readPacketLiteral(r)
+		if err != nil {
+			return 0, nil, err
+		}
+		p.Literal = v
+		read += n
+	default: // Operator packet
+		n, subpackets, err := readSubpackets(r)
+		if err != nil {
+			return 0, nil, err
+		}
+		p.Subpackets = subpackets
+		read += n
+	}
+
+	return read, p, nil
+}
+
+func readPacketLiteral(r *bitstream.BitReader) (int, uint64, error) {
+	read := 0
+
+	doRead := func() (stop bool, v uint64, err error) {
+		bits, err := r.ReadBits(5)
+		if err != nil {
+			return true, 0, err
+		}
+		cont := bits & 0b10000
+		value := bits & 0b1111
+		read += 5
+		return (cont == 0), value, nil
+	}
+
+	stop, v, err := doRead()
+	if err != nil {
+		return 0, 0, err
+	}
+	finalValue := v
+	for !stop {
+		finalValue = finalValue << 4
+		stop, v, err = doRead()
+		if err != nil {
+			return 0, 0, err
+		}
+		finalValue = finalValue | v
+	}
+	return read, finalValue, nil
+}
+
+func readSubpackets(r *bitstream.BitReader) (int, []*Packet, error) {
+	var subpackets []*Packet
+	read := 0
+	v, err := r.ReadBits(1)
+	if err != nil {
+		return 0, nil, err
+	}
+	read++
+	lt := PacketLengthType(v)
+	switch lt {
+	case PacketLengthTypeTotal:
+		v, err := r.ReadBits(15)
+		if err != nil {
+			return 0, nil, err
+		}
+		pLen := int(v)
+		read += 15
+		subread := 0
+		for subread < pLen {
+			n, sp, err := readPacket(r)
+			if err != nil {
+				return 0, nil, err
+			}
+			subpackets = append(subpackets, sp)
+			subread += n
+		}
+		read += subread
+
+	case PacketLengthTypeSubpackets:
+		v, err := r.ReadBits(11)
+		if err != nil {
+			return 0, nil, err
+		}
+		pLen := int(v)
+		read += 11
+		for i := 0; i < pLen; i++ {
+			n, sp, err := readPacket(r)
+			if err != nil {
+				return 0, nil, err
+			}
+			subpackets = append(subpackets, sp)
+			read += n
+		}
+	}
+
+	return read, subpackets, nil
+}
+
+func hexToBitstream(data []byte) (*bitstream.BitReader, error) {
+	b, err := hex.DecodeString(string(bytes.TrimSpace(data)))
+	if err != nil {
+		return nil, err
+	}
+	return bitstream.NewReader(bytes.NewReader(b)), nil
 }
--- 2021/day_16/main_test.go
+++ 2021/day_16/main_test.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"io"
 	"os"
+	"strings"
 	"testing"
 
+	bitstream "github.com/dgryski/go-bitstream"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -22,11 +25,185 @@ func getTestData(t *testing.T) []byte {
 }
 
 func Test_problemPart1(t *testing.T) {
-	got := problemPart1(getTestData(t))
-	assert.Equal(t, "NOT IMPLEMENTED", got)
+	tests := []struct {
+		name string
+		inp  string
+		want string
+	}{
+		{
+			name: "16",
+			inp:  "8A004A801A8002F478",
+			want: "16",
+		},
+		{
+			name: "12",
+			inp:  "620080001611562C8802118E34",
+			want: "12",
+		},
+		{
+			name: "23",
+			inp:  "C0015000016115A2E0802F182340",
+			want: "23",
+		},
+		{
+			name: "31",
+			inp:  "A0016C880162017C3686B18A3D4780",
+			want: "31",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := problemPart1([]byte(tt.inp))
+			assert.Equal(t, tt.want, got)
+		})
+	}
 }
 
 func Test_problemPart2(t *testing.T) {
-	got := problemPart2(getTestData(t))
-	assert.Equal(t, "NOT IMPLEMENTED", got)
+	tests := []struct {
+		name string
+		inp  string
+		want string
+	}{
+		{
+			name: "sum",
+			inp:  "C200B40A82",
+			want: "3",
+		},
+		{
+			name: "product",
+			inp:  "04005AC33890",
+			want: "54",
+		},
+		{
+			name: "min",
+			inp:  "880086C3E88112",
+			want: "7",
+		},
+		{
+			name: "max",
+			inp:  "CE00C43D881120",
+			want: "9",
+		},
+		{
+			name: "lt",
+			inp:  "D8005AC2A8F0",
+			want: "1",
+		},
+		{
+			name: "gt",
+			inp:  "F600BC2D8F",
+			want: "0",
+		},
+		{
+			name: "eq",
+			inp:  "9C005AC2F8F0",
+			want: "0",
+		},
+		{
+			name: "complex",
+			inp:  "9C0141080250320F1802104A08",
+			want: "1",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := problemPart2([]byte(tt.inp))
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_hexToBitstream(t *testing.T) {
+	bs, err := hexToBitstream([]byte("D2FE28"))
+	if !assert.NoError(t, err) {
+		return
+	}
+	got := strings.Builder{}
+	for {
+		b, err := bs.ReadBit()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			t.Fatal(err)
+		}
+		if b {
+			got.WriteByte('1')
+		} else {
+			got.WriteByte('0')
+		}
+	}
+	assert.Equal(t, "110100101111111000101000", got.String())
+}
+
+func Test_readPacketLiteral(t *testing.T) {
+	r := newBitstreamReader(t, 0b101111111000101000, 18)
+
+	n, got, err := readPacketLiteral(r)
+	if assert.NoError(t, err) {
+		assert.Equal(t, 15, n)
+		assert.EqualValues(t, 2021, got)
+	}
+}
+
+func newBitstreamReader(t *testing.T, u uint64, bits int) *bitstream.BitReader {
+	buf := bytes.Buffer{}
+	w := bitstream.NewWriter(&buf)
+	err := w.WriteBits(u, bits)
+	if err != nil {
+		t.Fatal(err)
+	}
+	r := bitstream.NewReader(bytes.NewReader(buf.Bytes()))
+	return r
+}
+
+func Test_readSubpackets(t *testing.T) {
+	type args struct {
+		r *bitstream.BitReader
+	}
+	tests := []struct {
+		name           string
+		args           args
+		wantN          int
+		wantSubpackets []*Packet
+		wantErr        bool
+	}{
+		{
+			name: "example",
+			args: args{
+				r: newBitstreamReader(t, 0b10000000001101010000001100100000100011000001100000, 50),
+			},
+			wantN: 45,
+			wantSubpackets: []*Packet{
+				{
+					Version: 2,
+					Type:    PacketTypeLiteral,
+					Literal: 1,
+				},
+				{
+					Version: 4,
+					Type:    PacketTypeLiteral,
+					Literal: 2,
+				},
+				{
+					Version: 1,
+					Type:    PacketTypeLiteral,
+					Literal: 3,
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotN, gotSubpackets, err := readSubpackets(tt.args.r)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, tt.wantN, gotN)
+			assert.Equal(t, tt.wantSubpackets, gotSubpackets)
+		})
+	}
 }
