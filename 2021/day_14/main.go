4fd058ce82aff8bd469d05f915509a518079eec1
Jaye Doepke <jtdoepke@protonmail.com>
2021-12-15 23:06:34 -0600

    2021 day 14

--- 2021/day_14/main.go
+++ 2021/day_14/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -22,9 +24,91 @@ func main() {
 }
 
 func problemPart1(inp []byte) string {
-	return "NOT IMPLEMENTED"
+	counts, pairs, pairInsertionRules := parseInput(inp)
+	counts, pairs = doPairInsertions(10, counts, pairs, pairInsertionRules)
+
+	var (
+		mostCommonCount  int
+		leastCommonCount = math.MaxInt
+	)
+	for _, count := range counts {
+		if count > mostCommonCount {
+			mostCommonCount = count
+		}
+		if count < leastCommonCount {
+			leastCommonCount = count
+		}
+	}
+
+	return strconv.Itoa(mostCommonCount - leastCommonCount)
 }
 
 func problemPart2(inp []byte) string {
-	return "NOT IMPLEMENTED"
+	counts, pairs, pairInsertionRules := parseInput(inp)
+	counts, pairs = doPairInsertions(40, counts, pairs, pairInsertionRules)
+
+	var (
+		mostCommonCount  int
+		leastCommonCount = math.MaxInt
+	)
+	for _, count := range counts {
+		if count > mostCommonCount {
+			mostCommonCount = count
+		}
+		if count < leastCommonCount {
+			leastCommonCount = count
+		}
+	}
+
+	return strconv.Itoa(mostCommonCount - leastCommonCount)
+}
+
+func doPairInsertions(n int, counts map[byte]int, pairs map[string]int, pairInsertionRules map[string]byte) (map[byte]int, map[string]int) {
+	for step := 0; step < n; step++ {
+		newPairs := make(map[string]int, len(pairs))
+		for pair, count := range pairs {
+			if insert, ok := pairInsertionRules[pair]; ok {
+				counts[insert] += count
+				newPairs[string(pair[0])+string(insert)] += count
+				newPairs[string(insert)+string(pair[1])] += count
+			} else {
+				newPairs[pair] += count
+			}
+		}
+		pairs = newPairs
+	}
+
+	var (
+		mostCommonCount  int
+		leastCommonCount = math.MaxInt
+	)
+	for _, count := range counts {
+		if count > mostCommonCount {
+			mostCommonCount = count
+		}
+		if count < leastCommonCount {
+			leastCommonCount = count
+		}
+	}
+
+	return counts, pairs
+}
+
+func parseInput(inp []byte) (elementCounts map[byte]int, pairs map[string]int, pairInsertionRules map[string]byte) {
+	lines := bytes.Split(bytes.TrimSpace(inp), []byte("\n"))
+	elementCounts = make(map[byte]int)
+	pairs = make(map[string]int)
+	for i := 0; i < len(lines[0])-1; i++ {
+		elementCounts[lines[0][i]]++
+		pairs[string(lines[0][i:i+2])]++
+	}
+	if len(lines[0]) > 1 {
+		elementCounts[lines[0][len(lines[0])-1]]++
+	}
+	pairInsertionRules = make(map[string]byte, len(lines)-2)
+	for i := 2; i < len(lines); i++ {
+		parts := bytes.Split(lines[i], []byte(" -> "))
+		pairInsertionRules[string(parts[0])] = parts[1][0]
+	}
+	return
 }
