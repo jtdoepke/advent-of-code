3c73f09adafc3dbb90430214725ff942f5b3ad01
Jaye Doepke <jtdoepke@protonmail.com>
2021-12-16 00:14:52 -0600

    2021 day 15

--- 2021/day_15/main.go
+++ 2021/day_15/main.go
@@ -6,6 +6,9 @@ import (
 	"io"
 	"log"
 	"os"
+	"strconv"
+
+	astar "github.com/beefsack/go-astar"
 )
 
 func main() {
@@ -22,9 +25,160 @@ func main() {
 }
 
 func problemPart1(inp []byte) string {
-	return "NOT IMPLEMENTED"
+	grid := parseInput(inp)
+	lastRow := grid[len(grid)-1]
+	path, distance, found := astar.Path(&grid[0][0], &lastRow[len(lastRow)-1])
+	if !found {
+		log.Panic("not found")
+	}
+	keep(path)
+	// for _, p := range path {
+	// 	t := p.(*Tile)
+	// 	fmt.Printf("%d,%d\n", t.x, t.y)
+	// }
+	return strconv.Itoa(int(distance))
 }
 
 func problemPart2(inp []byte) string {
-	return "NOT IMPLEMENTED"
+	grid := parseInput(inp)
+	grid = repeat(grid, 5, 5)
+	// fmt.Println(grid.String())
+	lastRow := grid[len(grid)-1]
+	path, distance, found := astar.Path(&grid[0][0], &lastRow[len(lastRow)-1])
+	if !found {
+		log.Panic("not found")
+	}
+	keep(path)
+	// for _, p := range path {
+	// 	t := p.(*Tile)
+	// 	fmt.Printf("%d,%d\n", t.x, t.y)
+	// }
+	return strconv.Itoa(int(distance))
+}
+
+type Tile struct {
+	grid [][]Tile
+	x, y int
+	risk int
+}
+
+func (t *Tile) PathNeighbors() (neighbors []astar.Pather) {
+	if t.x > 0 {
+		neighbors = append(neighbors, &t.grid[t.x-1][t.y])
+	}
+	if t.x < len(t.grid)-1 {
+		neighbors = append(neighbors, &t.grid[t.x+1][t.y])
+	}
+	if t.y > 0 {
+		neighbors = append(neighbors, &t.grid[t.x][t.y-1])
+	}
+	if t.y < len(t.grid[t.x])-1 {
+		neighbors = append(neighbors, &t.grid[t.x][t.y+1])
+	}
+	return
+}
+
+func (t *Tile) PathNeighborCost(to astar.Pather) float64 {
+	toTile := to.(*Tile)
+	return float64(toTile.risk)
+}
+
+func (t *Tile) PathEstimatedCost(to astar.Pather) float64 {
+	toTile := to.(*Tile)
+	return float64(manhattenDistance(t.x, toTile.x, t.y, toTile.y))
+}
+
+type Grid [][]Tile
+
+func (g Grid) String() string {
+	buf := bytes.NewBuffer(make([]byte, len(g)*len(g[0])+len(g)))
+	for _, row := range g {
+		for _, t := range row {
+			switch t.risk {
+			case 0:
+				buf.WriteByte('0')
+			case 1:
+				buf.WriteByte('1')
+			case 2:
+				buf.WriteByte('2')
+			case 3:
+				buf.WriteByte('3')
+			case 4:
+				buf.WriteByte('4')
+			case 5:
+				buf.WriteByte('5')
+			case 6:
+				buf.WriteByte('6')
+			case 7:
+				buf.WriteByte('7')
+			case 9:
+				buf.WriteByte('9')
+			case 8:
+				buf.WriteByte('8')
+			}
+		}
+		buf.WriteByte('\n')
+	}
+	return string(bytes.Trim(buf.Bytes(), "\n\x00"))
+}
+
+func parseInput(inp []byte) (grid Grid) {
+	lines := bytes.Split(bytes.TrimSpace(inp), []byte("\n"))
+	grid = make([][]Tile, len(lines))
+	for x, line := range lines {
+		grid[x] = make([]Tile, len(line))
+		for y, c := range line {
+			risk, err := strconv.Atoi(string(c))
+			if err != nil {
+				log.Panic(err)
+			}
+			grid[x][y] = Tile{
+				grid: grid,
+				x:    x,
+				y:    y,
+				risk: risk,
+			}
+		}
+	}
+	return
+}
+
+func keep(x interface{}) {
+	// I want to keep the debug logging commented out,
+	// but I don't want the Go compiler complaining about
+	// unused vars.
+}
+
+func repeat(grid Grid, timesX, timesY int) Grid {
+	origX, origY := len(grid), len(grid[0])
+	newGrid := make(Grid, origX*timesX)
+	for i := range newGrid {
+		newGrid[i] = make([]Tile, origY*timesY)
+	}
+	for x := range newGrid {
+		for y := range newGrid[x] {
+			risk := grid[x%origX][y%origY].risk
+			incBy := manhattenDistance(0, 0, x/origX, y/origY)
+			risk = (((risk - 1) + incBy) % 9) + 1
+			newGrid[x][y] = Tile{
+				grid: newGrid,
+				x:    x,
+				y:    y,
+				risk: risk,
+			}
+		}
+	}
+	return newGrid
+}
+
+func manhattenDistance(x1, y1, x2, y2 int) int {
+	absX := x1 - x2
+	if absX < 0 {
+		absX = -absX
+	}
+	absY := y1 - y2
+	if absY < 0 {
+		absY = -absY
+	}
+	return absX + absY
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require github.com/stretchr/testify v1.7.0
 
 require (
+	github.com/beefsack/go-astar v0.0.0-20200827232313-4ecf9e304482 // indirect
 	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
